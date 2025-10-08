80ded88e64bf1c9e884b3ee8426f6e23edabda96
nhanggwp <trungnhanspotify@gmail.com>
2025-10-09 04:21:17 +0700

    feat(ui): adjust main tab navigation for medical history

--- MediTrack/ClientApp/src/main/MainTab.js
+++ MediTrack/ClientApp/src/main/MainTab.js
@@ -3,7 +3,6 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image } from "react-native";
 import Home from "../home-tab/Home";
 import MedicalHistory from "../medical-hisory-tab/MedicalHistory";
-import KnowledgeBase from "../knowledge-base-tab/KnowledgeBase";
 import ProfileStack from "../profile-tab/ProfileStack";
 import Chat from "../chat-tab/Chat";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -13,7 +12,7 @@ const Tab = createBottomTabNavigator();
 
 const MainTab = () => {
   const route = useRoute();
-  const { token } = route.params || {};
+  const token = route.params?.token; // ✅ token tới từ LoginForm
 
   return (
     <Tab.Navigator
@@ -24,7 +23,6 @@ const MainTab = () => {
             const iconName = focused ? "chatbubble" : "chatbubble-outline";
             return <Ionicons name={iconName} size={size} color={color} />;
           }
-
           let iconSource;
           if (route.name === "Home") {
             iconSource = focused
@@ -34,25 +32,24 @@ const MainTab = () => {
             iconSource = focused
               ? require("./tab-icons/history-active.png")
               : require("./tab-icons/history.png");
-          } else if (route.name === "Knowledge Base") {
-            iconSource = focused
-              ? require("./tab-icons/knowledge-active.png")
-              : require("./tab-icons/knowledge.png");
           } else if (route.name === "Profile") {
             iconSource = focused
               ? require("./tab-icons/profile-active.png")
               : require("./tab-icons/profile.png");
           }
-
           return (
             <Image source={iconSource} style={{ width: size, height: size }} />
           );
         },
       })}
     >
+      {/* ✅ Pass token xuống Home & MedicalHistory qua initialParams */}
       <Tab.Screen name="Home" component={Home} initialParams={{ token }} />
-      <Tab.Screen name="Medical History" component={MedicalHistory} />
-      {/* <Tab.Screen name="Knowledge Base" component={KnowledgeBase} /> */}
+      <Tab.Screen
+        name="Medical History"
+        component={MedicalHistory}
+        initialParams={{ token }}
+      />
       <Tab.Screen name="Chat" component={Chat} />
       <Tab.Screen name="Profile" component={ProfileStack} />
     </Tab.Navigator>
